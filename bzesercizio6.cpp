f95b7ffa466ba3e3752e2ceb1e69885ab90f05d1
BEATRICE ZILIANI <73991092+beatriceziliani@users.noreply.github.com>
2021-11-11 10:50:13 +0100

    Add files via upload

---
+++ bzesercizio6.cpp
@@ -0,0 +1,16 @@
+/*L’utente inserisce un numero naturale in input. Il programma deve stabilire se il numero è PRIMO.*/
+#include <stdio.h>
+
+int main(){
+    int num_n = 0;
+    int i = 2;
+    
+    printf("Inserisci un numero naturale e ti dirò se è primo ");
+    scanf("%d", &num_n);
+    for(i; i< num_n; i++){
+    	
+    		if (num_n/num_n || num_n/1){
+		printf("%d\n",num_n);
+	}
+
+}
