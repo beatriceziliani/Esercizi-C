4b5d612b5424e9224939b32a80cb4dda6b05bacd
BEATRICE ZILIANI <73991092+beatriceziliani@users.noreply.github.com>
2021-11-25 10:40:11 +0100

    Create esercizioprova.cpp

---
+++ esercizioprova.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int n = 0;
+int d = 0;
+int i = 1;
+int a = 1;
+int somma = 0;
+
+printf("Di quanti numeri dispari vuoi la somma?");
+scanf("%d", &n);
+
+for(i; i<=n; i ++){
+    d = a + 2;
+    somma = 
+}
+
+    return 0;
+}
